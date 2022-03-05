95e9084d9d25c6e19683c81a231119e73b8ed051
Jesús Silva Méndez <jesussilva@Jesus-Silva.local>
2022-03-05 17:59:13 +0100

    fix(format-response): formatting has been corrected

--- packages/api/src/utils/format-response.ts
+++ packages/api/src/utils/format-response.ts
@@ -2,8 +2,8 @@ import { plainToInstance } from 'class-transformer'
 
 export function FormatResponse(model: any, response: any) {
   if (Array.isArray(response)) {
-    return response.map((item: any) => plainToInstance(model, item))
+    return response.map((item: any) => plainToInstance(model, JSON.parse(JSON.stringify(item))))
   } else {
-    return plainToInstance(model, response)
+    return plainToInstance(model, JSON.parse(JSON.stringify(response)))
   }
 }
