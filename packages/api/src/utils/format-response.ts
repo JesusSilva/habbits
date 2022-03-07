c8e1c5094933c071ebe553ae7b638ff6bbbd9c0a
Jesús Silva Méndez <jesussilva@Jesus-Silva.local>
2022-03-07 20:37:05 +0100

    fix(format-response): formatting has been fixed to models Diet and Training

--- packages/api/src/utils/format-response.ts
+++ packages/api/src/utils/format-response.ts
@@ -1,8 +1,34 @@
 import { plainToInstance } from 'class-transformer'
+import { DayClass } from '../controlers/day/Day'
+import { DietClass } from '../controlers/diet/Diet'
+import { ExerciseClass } from '../controlers/exercise/Exercise'
+import { TrainingClass } from '../controlers/training/Training'
 
-export function FormatResponse(model: any, response: any) {
+export function FormatResponse(model: any, response: any, type: string = 'normal') {
   if (Array.isArray(response)) {
-    return response.map((item: any) => plainToInstance(model, JSON.parse(JSON.stringify(item))))
+    if (type === 'diet') {
+      response = response as DietClass[]
+      response = response.map((diet: any) => {
+        diet = plainToInstance(DietClass, JSON.parse(JSON.stringify(diet)))
+        diet.days = diet.days.map((day: DayClass) => plainToInstance(DayClass, JSON.parse(JSON.stringify(day))))
+        return diet
+      })
+
+      return response
+    } else if (type === 'training') {
+      response = response as TrainingClass[]
+      response = response.map((training: any) => {
+        training = plainToInstance(TrainingClass, JSON.parse(JSON.stringify(training)))
+        training.exercises = training.exercises.map((exercises: ExerciseClass) =>
+          plainToInstance(ExerciseClass, JSON.parse(JSON.stringify(exercises)))
+        )
+        return training
+      })
+
+      return response
+    } else if (type === 'normal') {
+      return response.map((item: any) => plainToInstance(model, JSON.parse(JSON.stringify(item))))
+    }
   } else {
     return plainToInstance(model, JSON.parse(JSON.stringify(response)))
   }
